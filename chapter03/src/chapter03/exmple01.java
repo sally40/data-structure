3426bbf433ca708b492d1aef0334e525c7d7b622
seodahyun <seodahyun@218.38.137.28>
2021-12-22 09:30:25 +0900

    Third Commit

---
+++ chapter03/src/chapter03/exmple01.java
@@ -0,0 +1,10 @@
+package chapter03;
+
+class  example01{
+	public  static void main(String srgs[]){
+	     int sale[] = new int[]{157, 209, 251, 312};
+
+ 	     for(int i=0; i<4; i++)
+	        System.out.printf("%d/4분기 : sale[%d]= %d %n", i+1, i, sale[i]);
+        }
+}
