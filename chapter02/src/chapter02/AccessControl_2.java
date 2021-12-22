654582bebd93cb26bd78140b2182d64f033bec24
seodahyun <seodahyun@218.38.137.28>
2021-12-22 09:23:09 +0900

    Second Commit

---
+++ chapter02/src/chapter02/AccessControl_2.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+class AccessControl_2{
+    private int num1, num2, sum;
+    private double ave;
+    AccessControl_2(int a, int b){
+	this.num1 = a;
+	this.num2 = b;
+    }
+    public int getSum(){
+ 	sum = num1 + num2;
+	return sum;
+    }
+    public double getAve(){
+	ave = sum/2.0;
+	return ave;
+    }
+}
---
+++ chapter02/src/chapter02/AccessControl_3.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+class AccessControl_3{
+    private int num1, num2, sum;
+    private double ave;
+    AccessControl_3(int a, int b){
+	this.num1 = a;
+	this.num2 = b;
+    }
+    private int getSum(){
+ 	sum = num1 + num2;
+	return sum;
+    }
+    private double getAve(){
+	ave = sum/2;
+	return ave;
+    }
+}
